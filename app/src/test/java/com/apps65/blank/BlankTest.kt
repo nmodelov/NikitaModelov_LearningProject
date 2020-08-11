d460c9ca087559b4f5b314a3c437dfeb7e38b3cf
Roman Pozdeev <rpozdeev@65apps.com>
2020-08-11 13:13:12 +0400

    coroutines test example

---
+++ app/src/test/java/com/apps65/blank/BlankTest.kt
@@ -0,0 +1,40 @@
+package com.apps65.blank
+
+import com.apps65.mvi.saving.SavedStateKeeperImpl
+import com.apps65.mvitemplate.domain.blank.store.BlankStore
+import com.apps65.mvitemplate.domain.blank.store.BlankStoreFactory
+import com.arkivanov.mvikotlin.core.utils.isAssertOnMainThreadEnabled
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.assertj.core.api.Assertions.assertThat
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object BlankTest : Spek({
+    beforeEachGroup {
+        isAssertOnMainThreadEnabled = false
+        Dispatchers.setMain(newSingleThreadContext("Test UI thread"))
+    }
+
+    describe("blank screen test") {
+        val blankStoreFactory = BlankStoreFactory(DefaultStoreFactory, SavedStateKeeperImpl())
+        val blankStore by memoized { blankStoreFactory.create() }
+
+        it("should be 1 after increment") {
+            blankStore.accept(BlankStore.Intent.Blank)
+            runBlocking(Dispatchers.Main) {
+                val currentState = blankStore.state as BlankStore.State.Blank
+                assertThat(currentState.blankCount).isEqualTo(1)
+            }
+        }
+    }
+
+    afterEachGroup {
+        isAssertOnMainThreadEnabled = true
+        Dispatchers.resetMain()
+    }
+})

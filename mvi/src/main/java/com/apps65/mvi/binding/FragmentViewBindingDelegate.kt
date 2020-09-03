c3520eac800158f384b2b790216498aeb1bcfd96
Roman Pozdeev <rpozdeev@65apps.com>
2020-09-03 12:59:21 +0400

    FragmentViewBindingDelegate cleanup

--- mvi/src/main/java/com/apps65/mvi/binding/FragmentViewBindingDelegate.kt
+++ mvi/src/main/java/com/apps65/mvi/binding/FragmentViewBindingDelegate.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.observe
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class FragmentViewBindingDelegate<T : ViewBinding>(
-    val fragment: Fragment,
+    fragment: Fragment,
     val viewBindingFactory: (View) -> T
 ) : ReadOnlyProperty<Fragment, T> {
     private var binding: T? = null
@@ -31,14 +30,13 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
     }
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-        val binding = binding
-        if (binding != null) {
-            return binding
+        binding?.let {
+            return it
         }
 
-        val lifecycle = fragment.viewLifecycleOwner.lifecycle
-        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
-            throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
+        val lifecycle = thisRef.viewLifecycleOwner.lifecycle
+        check(lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            "Should not attempt to get bindings when Fragment views are destroyed."
         }
 
         return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
